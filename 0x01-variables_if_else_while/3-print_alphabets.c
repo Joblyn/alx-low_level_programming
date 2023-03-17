70252b43513fcdd2f1ea958a92c5611671b12014
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-03-17 13:18:32 -0700

    adding 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Write all alphabets in lowercase, then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_case = 'a';
+	char upper_case = 'A';
+
+	while (lower_case <= 'z')
+	{
+		putchar(lower_case);
+		lower_case = lower_case + 1;
+	}
+
+	while (upper_case <= 'Z')
+	{
+		putchar(upper_case);
+		upper_case = upper_case + 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
