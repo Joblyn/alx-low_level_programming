f438b31536f7feee511046080bd2be4b82ac448c
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-03-21 14:51:47 -0700

    adding 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Print 1 if character is alphabetic, else 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for alphabetic character. 0 for non-alphabetic
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
