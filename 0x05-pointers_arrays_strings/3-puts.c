fa7feb46992235f167224580a14627c44a6814a1
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-03-27 19:57:47 -0700

    adding 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdou
+ *
+ * @str: string parameter
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		i++;
+	}
+}
