b8febb01cd2b9881cf85df73662dfa5a8efae709
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-03-30 16:53:43 -0700

    adding 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string to be appended to
+ * @src: string to be appended
+ * @n: maximum bytes
+ *
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (*(src + j) != '\0')
+	{
+		if (j == n)
+			break;
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
