f5165716b7d4a5696ac823dc3cdd9ef60a1b63ba
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-04-24 09:39:48 -0700

    fix bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		if (i == n - 1)
-			printf("%d\n", va_arg(ap, int));
-		else if (separator)
-			printf("%d%s", va_arg(ap, int), separator);
-
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
 	va_end(ap);
 }
