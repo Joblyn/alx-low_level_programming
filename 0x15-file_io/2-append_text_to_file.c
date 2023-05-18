fafd0c69d6f9ad06983b0125be6af5cf6feeca2b
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-05-18 11:35:22 -0700

    adding 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of file
+ * @text_content: string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_d;
+	int n_letters;
+	int file_wr;
+
+	if (!filename)
+		return (-1);
+
+	file_d = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (n_letters = 0; text_content[n_letters]; n_letters++)
+			;
+
+		file_wr = write(file_d, text_content, n_letters);
+
+		if (file_wr == -1)
+			return (-1);
+	}
+
+	close(file_d);
+
+	return (1);
+}
