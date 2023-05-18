3d50eaecbbd7c0d7e131b61247e5741f4aac78f8
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-05-18 11:22:32 -0700

    adding 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_d;
+	int n_letters;
+	int file_wr;
+
+	if (!filename)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (n_letters = 0; text_content[n_letters]; n_letters++)
+		;
+
+	file_wr = write(file_d, text_content, n_letters);
+
+	if (file_wr == -1)
+		return (-1);
+
+	close(file_d);
+
+	return (1);
+}
